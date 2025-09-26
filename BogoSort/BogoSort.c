86ebc6de2d13eae0a811ec640e3e12d75e1710e0
Citizen7751 <citizen7751@proton.me>
2025-09-26 10:54:46 +0200

    All programs got a little polishing and now they all run on C89 and C2x as well

--- BogoSort/BogoSort.c
+++ BogoSort/BogoSort.c
@@ -1,95 +1,96 @@
-//MIT License
-//Copyright (c) 2024 Citizen7751
+/* MIT License
+   Copyright (c) 2024- Citizen7751
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 
+typedef unsigned int Uint;
+typedef unsigned char Bool;
+#define true  1
+#define false 0
+
+
 #ifdef _WIN32
   #include <windows.h>
 #elif __linux__
   #include <unistd.h>
-  void Sleep(unsigned int d) {
-     usleep(d*1000);
+  void Sleep(Uint d) {
+     usleep(d * 1000);
   }
 #endif
 
-typedef unsigned int uint;
-typedef int type;
-/* "type" is defined like this
-   for ease of change.
 
-   Don't forget to change
-   the format specifier in
-   the "print_elements" function
-   when changing the type!
-*/
-
-int is_sorted(type* arr, uint* l) {
-    for (uint i=1; i<*l; i++)
-        if (arr[i-1] > arr[i]) return 0;
-    return 1;
+Bool is_sorted(double* arr, const Uint* l) {
+	Uint i;
+    for (i = 1; i < *l; i++)
+        if (arr[i-1] > arr[i]) return false;
+    return true;
 }
 
-void swap(type* a, type* b) {
-    type c = *a;
+void swap(double* a, double* b) {
+    double c = *a;
     *a = *b;
     *b = c;
 }
 
-void mix_elements(type* arr, uint* l) {
-    srand(time(NULL));
-    int i1, i2;
-    for (uint i=0; i<*l; i++) {
-        i1 = rand()%*l;
-        i2 = rand()%*l;
+void mix_elements(double* arr, const Uint* l) {
+
+    Uint i1, i2;
+	Uint i;
+    for (i = 0; i < *l; i++) {
+        i1 = rand() % *l;
+        i2 = rand() % *l;
         swap(&arr[i1], &arr[i2]);
     }
 }
 
-void print_elements_nl(type* arr, uint* l) {
-    for (uint i=0; i<*l; i++)
-        printf("%d ", arr[i]);
-    putchar('\n');
-}
-
-void print_elements_cr(type* arr, uint* l) {
-    for (uint i=0; i<*l; i++)
-        printf("%d ", arr[i]);
-    putchar('\r');
+void print_elements(double* arr, const Uint* l, const char endc) {
+	Uint i;
+    for (i = 0; i < *l; i++)
+        printf("%lf ", arr[i]);
+    putchar(endc);
 }
 
 
-//--------------------------------------------
+/* -------------------------------------------- */
 
-void BogoSort_Verbose(type* arr, uint* l, uint* i) {
+void BogoSort_Verbose(double* arr, const Uint* l, Uint* i) {
     while(!is_sorted(arr, l)) {
         mix_elements(arr, l);
         (*i)++;
-        print_elements_cr(arr, l);
+        print_elements(arr, l, '\r');
         Sleep(300);
     }
 }
 
-void BogoSort_Quiet(type* arr, uint* l, uint* i) {
+void BogoSort_Quiet(double* arr, const Uint* l, Uint* i) {
     while (!is_sorted(arr, l)) {
         mix_elements(arr, l);
         (*i)++;
     }
 }
 
-//--------------------------------------------
+/* -------------------------------------------- */
 
 int main(void) {
 
-    type arr[] = {3, 8, 2, 1};
-    uint length = sizeof(arr)/sizeof(arr[0]);
-    uint iterations = 0;
+    double arr[] = { 3.501, 8.9843, 2.2114, 1.9981 };
+    const Uint length = sizeof(arr)/sizeof(arr[0]);
+    Uint iterations = 0;
+
+    srand(time(NULL));
+    
     printf("Original arrangement:\n");
-    print_elements_nl(arr, &length);
+    print_elements(arr, &length, '\n');
 
     BogoSort_Verbose(arr, &length, &iterations);
-    //BogoSort_Quiet(arr, &length, &iterations);
+    /* BogoSort_Quiet(arr, &length, &iterations); */
+
+	printf("\nSorted arrangement:\n");
+    print_elements(arr, &length, '\n');
+
 
     printf("\nDone in %u iterations(s).\nPress Enter to exit.\n", iterations);
     while (getchar()!='\n');
--- FizzBuzz/FizzBuzz.c
+++ FizzBuzz/FizzBuzz.c
@@ -1,46 +1,56 @@
-//MIT License
-//Copyright (c) 2025 Citizen7751
+/* MIT License
+   Copyright (c) 2025- Citizen7751
+*/
 
 #include <stdio.h>
 #include <math.h>
 
 typedef struct {
-    int n;
-    char word[11];
+    const int n;
+    const char word[16];
 } Condition;
 
-//---------------------------------------
-//---------- Configurable part ----------
+typedef unsigned char Bool;
+#define true  1
+#define false 0
+
+
+/* ---------- Configurable part --------- */
+
 const int BEGIN = 100;
-const int END = 1;
-const int PACE = -2;
+const int END = -10;
+const int PACE = -3;
 
 const Condition conditions[] = {
     {3, "Fizz"},
     {5, "Buzz"},
     {7, "Bazz"},
 };
-//---------------------------------------
-//---------------------------------------
 
-int check_and_print_on(const int i) {
-    int is_divisible = 0;
-    for (unsigned long long ci = 0; ci < sizeof(conditions)/sizeof(conditions[0]); ci++)
+/*---------------------------------------*/
+
+
+Bool check_and_print_on(const int i) {
+    Bool is_divisible = false;
+	unsigned long long ci;
+
+    for (ci = 0; ci < sizeof(conditions) / sizeof(conditions[0]); ci++)
         if (i % conditions[ci].n == 0) {
-            is_divisible = 1;
+            is_divisible = true;
             printf("%s", conditions[ci].word);
         }
     return is_divisible;
 }
 
-void FizzBuzz() {
-    for (int i = BEGIN; i != END+(PACE/abs(PACE)); i+= PACE) {
+void FizzBuzz(void) {
+	int i;
+    for (i = BEGIN; i != END + (PACE / abs(PACE)); i += PACE) {
         if (!check_and_print_on(i)) printf("%d", i);
         putchar('\n');
     }
 }
 
-int main() {
+int main(void) {
     FizzBuzz();
     while(getchar()!='\n');
     return 0;
